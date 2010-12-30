9ff2d48336a840e83df075a76553d73cb5dd0532
ansanto <ansanto@80dc9642-2cd0-42e7-ba37-ca11138e62bc>
2010-12-30 14:46:50 +0000

    use new version string: backfire-revision_robin-build

--- packages/robin-v2/files/lib/robin/preup/35_banner.sh
+++ packages/robin-v2/files/lib/robin/preup/35_banner.sh
@@ -2,16 +2,16 @@
 
 ROBIN_BUILD=$(cat /etc/robin_build)
 BACKFIRE=$(cat /etc/openwrt_svn_rev)
+ROBIN_VERSION="${BACKFIRE}_${ROBIN_BUILD}"
 
-echo "r${ROBIN_BUILD}" > /etc/robin_version
-
+echo "r${ROBIN_VERSION}" > /etc/robin_version
 cat > /etc/banner << banner_end
   ______         __     __         _______                __    
  |   __ \.-----.|  |--.|__|.-----.|   |   |.-----..-----.|  |--.
  |      <|  _  ||  _  ||  ||     ||       ||  -__||__ --||     |
  |___|__||_____||_____||__||__|__||__|_|__||_____||_____||__|__|
  Robin2-802.11n                                  
- bleeding edge, r$ROBIN_BUILD - Openwrt backfire, r$BACKFIRE
+ bleeding edge, "r${BACKFIRE}_${ROBIN_BUILD}"
  --------------------------------------------------------------- 
  Powered by these open source projects:
  http://www.openwrt.org     http://www.open-mesh.org
